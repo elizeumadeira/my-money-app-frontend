96a2ce2f28f907c339f27de0e1142c25a7e297cf
Elizeu Santos Madeira <elizeu.madeira@gmail.com>
2019-01-12 19:44:07 -0200

    modificações package.json

--- src/index.js
+++ src/index.js
@@ -8,14 +8,14 @@ import promise from 'redux-promise';
 import multi from 'redux-multi';
 import thunk from 'redux-thunk';
 
-import Routes from './main/routes';
+import AuthOrApp from './main/authOrApp';
 import reducers from './main/reducers';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = applyMiddleware(promise, multi, thunk)(createStore)(reducers, devTools);
 ReactDOM.render(
     <Provider store={store}>
-        <Routes />
+        <AuthOrApp />
     </Provider>
     , document.getElementById('app'));
 
--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
+// import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router';
 
 import AuthOrApp from './authOrApp';
 import Dashboard2 from '../dashboard2/dashboard2';
 import BillingCycle from '../billingCycle/billingCycle';
 
 export default props => (
-    <Router history={hashHistory}>
-        <Route path='/' component={AuthOrApp}>
-            <IndexRoute component={Dashboard2} />
-            <Route path='billingCycles' component={BillingCycle} />
-        </Route>
-        <Redirect from='*' to="/" />
-    </Router>
+    <div className='content-wrapper'>
+        <Switch>
+            <Route exact path='/' component={Dashboard2} />
+            <Route path='/billingCycles' component={BillingCycle} />
+            <Redirect from='*' to="/" />
+        </Switch>
+    </div>
+
+    // <Router history={hashHistory}>
+    //     <Route path='/' component={AuthOrApp}>
+    //         <IndexRoute component={Dashboard2} />
+    //         <Route path='billingCycles' component={BillingCycle} />
+    //     </Route>
+    //     <Redirect from='*' to="/" />
+    // </Router>
 )
